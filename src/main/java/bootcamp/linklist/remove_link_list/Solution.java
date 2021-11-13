a73fc7fb77b8efa5d036d0890c5e8ea69bf64c0b
Arockia Akilan Irudayaraja <arockiaakilanirudayaraja@Arockias-Mac-mini.local>
2021-11-13 13:44:25 +0800

    Additional comments for remove list

--- src/main/java/bootcamp/linklist/remove_link_list/Solution.java
+++ src/main/java/bootcamp/linklist/remove_link_list/Solution.java
@@ -4,20 +4,79 @@ import bootcamp.ListNode;
 
 public class Solution {
 
+    /**
+    *
+    * Sentinel node is key
+    * Sentinel node next will point to head.
+    * prev and curr pointers will run thru the list.
+    * when curr points the removal target, point prev.next to curr.next
+    * sentinel.next points to root.
+     * ote:refer examples
+    *
+    * */
+
     public static void main(String[] args) {
-        ListNode listNode = new Solution().removeElements(ListNode.arrayToList(1, 1, 1, 3), 1);
-        while (listNode!=null){
+        ListNode listNode = new Solution().removeElements(ListNode.arrayToList(1, 2, 1, 3), 1);
+        while (listNode != null) {
             System.out.println(listNode.val);
             listNode = listNode.next;
         }
 
     }
 
+    /**
+     * Iteration 1
+     * head -> 1 -> 1-> 1-> 3
+     * sentinel.next -> 1 -> 1-> 1-> 3
+     * prev = null -> 1 -> 1-> 1-> 3
+     * curr -> 1 -> 1-> 1-> 3
+     *
+     *
+     * Iteration 2
+     *
+     * head -> 1 -> 1-> 1-> 3
+     * sentinel.next  -> 1-> 1-> 3
+     * prev = (null)  -> 1-> 1-> 3
+     *
+     *
+     */
+
+
+    /**
+     * Iteration 1
+     * head -> 1 -> 2-> 1-> 3
+     * sentinel.next -> 1 -> 2-> 1-> 3
+     * prev = null -> 1 -> 2-> 1-> 3
+     * curr -> 1 -> 2-> 1-> 3
+     *
+     *
+     * Iteration 2
+     *
+     * head -> 1 -> 2-> 1-> 3
+     * sentinel.next  -> 2-> 1-> 3
+     * prev = (null)  -> 2-> 1-> 3
+     * cur -> 2 -> 1-> 3
+     *
+     * Iteration 3
+     * head -> 1 -> 2-> 1-> 3
+     * sentinel.next  -> 2-> 1 -> 3
+     * prev = (2) -> 1 -> 3
+     * cur -> 1 -> 3
+     *
+     * Iteration 4
+     * head -> 1 -> 2->  3
+     * sentinel.next  -> 2 -> 3
+     * prev = (2)  -> 3
+     * cur = 3
+     */
+
+
+
     public ListNode removeElements(ListNode head, int val) {
-        ListNode sentinal = new ListNode();
-        sentinal.next = head;
+        ListNode sentinel = new ListNode();
+        sentinel.next = head;
         ListNode curr = head;
-        ListNode prev = sentinal;
+        ListNode prev = sentinel;
         while (curr != null) {
             if (curr.val == val) {
                 prev.next = curr.next;
@@ -26,7 +85,7 @@ public class Solution {
             }
             curr = curr.next;
         }
-        return sentinal.next;
+        return sentinel.next;
     }
 
 }
